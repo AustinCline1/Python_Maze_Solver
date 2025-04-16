28411966c27b13726d84349cdc63eb3b9d0dc547
Austin Cline <au.cline@hotmail.com>
2025-04-16 18:44:56 -0400

    Added a cell class and updated the window class to have points and lines

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
-from src.window import Window
-
+from src.window import Window, Line, Point
+from src.cell import Cell
 def main():
     win = Window(800, 600)
+    c = Cell(win)
+    c.has_right_wall = False
+    c.draw(50,50,100,100)
+
+    c2 = Cell(win)
+    c2.has_left_wall = False
+    c2.has_bottom = False
+    c2.draw(100,50,150,100)
+
+    c.draw_move(c2)
+
     win.wait_for_close()
 
 
--- src/window.py
+++ src/window.py
@@ -9,7 +9,7 @@ class Window:
         self.root.title("Maze Solver")
         self.root.protocol("WM_DELETE_WINDOW", self.close)
         self.canvas = Canvas(self.root, width=self.width, height=self.height)
-        self.canvas.pack()
+        self.canvas.pack(fill=BOTH, expand=True)
         self.running = False
 
 
@@ -24,3 +24,20 @@ class Window:
 
     def close(self):
         self.running = False
+
+    def draw_line(self, line, fill_color):
+        line.draw(self.canvas, fill_color)
+
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Line:
+    def __init__(self, point1, point2):
+        self.point1 = point1
+        self.point2 = point2
+
+    def draw(self, canvas, fill_color="blue"):
+        canvas.create_line(self.point1.x, self.point1.y, self.point2.x, self.point2.y, fill=fill_color, width=2)
